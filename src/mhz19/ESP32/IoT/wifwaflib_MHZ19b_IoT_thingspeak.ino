dca439097cefec4aee4bf51a548fe21def8d3b19
etodorovich <elias.todorovich@gmail.com>
2021-09-04 19:40:43 -0300

    Uso de librería thingspeak

--- src/mhz19/ESP32/IoT/wifwaflib_MHZ19b_IoT_thingspeak.ino
+++ src/mhz19/ESP32/IoT/wifwaflib_MHZ19b_IoT_thingspeak.ino
@@ -1,8 +1,11 @@
 /*
   wifwaflib_MHZ19b_IoT_thingspeak.ino - Program for ESP32 and MH-Z19b 
      CO2 sensor. Connects to the IoT thingspeak platform.  
-     This program uses the library from
+     This program uses the libraries from
        https://github.com/WifWaf/MH-Z19
+       https://github.com/mathworks/thingspeak-arduino
+     Both available in Arduino too.
+       
      Warning level signal RGB LED
      Push Button for configuation control
        (1) If the button is held down for less than 
@@ -10,7 +13,7 @@
        (2) if the duration is longer than one second, 
            the device goes to calibration mode.
 
-  Copyright (c) 2021 Elías Todorovich
+  Copyright (c) 2021 Elías Todorovich, Matías Presso
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -45,6 +48,9 @@ int CO2_base = 400;
 
 const int samplingPeriod = 15000; // 15 seconds
 
+int d=0; 
+const int S=3; // Samples per telemetry recording
+
 /*----------------------------------------------------------
     MH-Z19 CO2 sensor: UART2 in a ESP32s SoC
   ----------------------------------------------------------*/
@@ -61,21 +67,28 @@ const int tx2_pin = 17;	//Serial tx1 pin no
 #endif
 
 /*----------------------------------------------------------
-  Basic WiFi Server in a ESP32s SoC 
+  WiFi connection in a ESP32s 
   ----------------------------------------------------------*/
 #include <WiFi.h>
+#include "secrets.h"
 
-const char* ssid     = "***********"; // Your WiFi
-const char* password = "***********";             // Your password
+char ssid[] = SECRET_SSID;       // your network SSID (name) 
+char password[] = SECRET_PASS;   // your network password
+
+WiFiClient myClient;
 
 /*----------------------------------------------------------
   ThingSpeak settings
   ----------------------------------------------------------*/
+#include "ThingSpeak.h"
+  
 char server[] = "api.thingspeak.com";
-String writeAPIKey = "****************"; // Your Write API key at thingspeak 
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char* myWriteAPIKey = SECRET_WRITE_APIKEY;
+//String myWriteAPIKey = SECRET_WRITE_APIKEY;
 
 /*----------------------------------------------------------
-  RGB LED in a NodeMCU ESP32s SoC 
+  RGB LED in a ESP32
   ----------------------------------------------------------*/
 const int led_R   = 12; 
 const int PWMR_Ch = 0;
@@ -118,8 +131,6 @@ void IRAM_ATTR isr_button() {
     portENTER_CRITICAL(&mux);
     if (!button1.down){
       button1.down = true;
-      //setRGB_LEDColor (0, 0, 255);  // Blue means warming or Configuring:
-                                    //   baseline setting or calibrating
     } else { 
       button1.down = false;
       button1.timePressed = nowMillis - lastMillis;
@@ -146,15 +157,15 @@ void retrieveInfo_mhz19 () {
     if(i == 1)
       Serial.print(".");    
   }
-   Serial.println("");
+  Serial.println("");
 
-   Serial.print("Range: ");
-   Serial.println(mhz19.getRange());   
-   Serial.print("Background CO2: ");
-   Serial.println(mhz19.getBackgroundCO2());
-   Serial.print("Temperature Cal: ");
-   Serial.println(mhz19.getTempAdjustment());
-   Serial.print("ABC Status: "); mhz19.getABC() ? Serial.println("ON") :  Serial.println("OFF");
+  Serial.print("Range: ");
+  Serial.println(mhz19.getRange());   
+  Serial.print("Background CO2: ");
+  Serial.println(mhz19.getBackgroundCO2());
+  Serial.print("Temperature Cal: ");
+  Serial.println(mhz19.getTempAdjustment());
+  Serial.print("ABC Status: "); mhz19.getABC() ? Serial.println("ON") :  Serial.println("OFF");
 } 
 
 /*----------------------------------------------------------
@@ -164,6 +175,7 @@ void connectWiFi(){
 
     while (WiFi.status() != WL_CONNECTED){
         WiFi.begin(ssid, password);
+        Serial.print(".");
         delay(3000);
     }
 
@@ -197,11 +209,12 @@ void setup() {
   setRGB_LEDColor (0, 0, 255);  // Blue means warming or Configuring:
                                 //   baseline setting or calibrating
 
-  //pinMode(button1.PIN, INPUT_PULLUP); Noisy Pushes
-  pinMode(button1.PIN, INPUT);// Modified due to Noisy Pushes
+  pinMode(button1.PIN, INPUT);
   attachInterrupt(button1.PIN, isr_button, CHANGE);
 
   connectWiFi();
+
+  ThingSpeak.begin(myClient);
 
   delay (180000); // Wait 3 minutes for warming purposes
 
@@ -251,55 +264,14 @@ void calibrate_mhz19()
 }
 
 /*----------------------------------------------------------
-   Connect to the ThingSpeak server and build the data strings 
-   for the HTTP POST command.
-  ----------------------------------------------------------*/
-void httpRequest(int c, int t) {
-
-    WiFiClient client;
-    
-    if (!client.connect(server, 80)){
-      
-        Serial.println("Connection failed");
-        //lastConnectionTime = millis();
-        client.stop();
-        return;     
-    }
-    
-    else{
-        
-        // Create data string to send to ThingSpeak.
-        String data = "field1=" + String(c) + "&field2=" + String(t);         
-        // POST data to ThingSpeak.
-        if (client.connect(server, 80)) {
-          
-            client.println("POST /update HTTP/1.1");
-            client.println("Host: api.thingspeak.com");
-            client.println("Connection: close");
-            client.println("User-Agent: ESP32WiFi/1.1");
-            client.println("X-THINGSPEAKAPIKEY: "+writeAPIKey);
-            client.println("Content-Type: application/x-www-form-urlencoded");
-            client.print("Content-Length: ");
-            client.print(data.length());
-            client.print("\n\n");
-            client.print(data);
-            
-            //lastConnectionTime = millis();   
-            delay(250);
-        }
-    }
-    client.stop();
-}
-
-/*----------------------------------------------------------
     MH-Z19 CO2 sensor loop
   ----------------------------------------------------------*/
 void loop() {
   int co2ppm = mhz19.getCO2();          // Request CO2 (as ppm)
-  int temp = mhz19.getTemperature();    // Request Temperature (as Celsius)
+  int temp = mhz19.getTemperature();    // Request Temperature (as Celsius)                          
 
   if (button1.event) {
-      Serial.printf("Button has been pressed for %u millis\n", button1.timePressed);
+      //Serial.printf("Button has been pressed for %u millis\n", button1.timePressed);
       if (button1.timePressed < 1000)
         CO2_base = co2ppm;
       else calibrate_mhz19 ();
@@ -317,12 +289,21 @@ void loop() {
   Serial.print(","); 
   Serial.println(temp);
 
+  if (d%S==0){     // Telemetry is sent every S samplingPeriod seconds
   // Measurements to WiFi thingspeak IoT Platform 
   //
-  if (WiFi.status() != WL_CONNECTED) connectWiFi();
+   if (WiFi.status() != WL_CONNECTED) connectWiFi();
 
-  httpRequest(co2ppm, temp);
+   ThingSpeak.setField (1,co2ppm);
+   ThingSpeak.setField (2,temp);
 
+   ThingSpeak.writeFields(myChannelNumber,myWriteAPIKey);
+   Serial.println("Datos enviados a ThingSpeak.");   
+  }
+  d++;
+
+  // Feedback to RGB LED
+  //
   if (!button1.down) 
     // CO2 measurement to RGB LED
     if (co2ppm < CO2_base+300) {
