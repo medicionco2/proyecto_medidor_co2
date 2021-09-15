417a3b27f228f5619c27807edac491c884394341
etodorovich <elias.todorovich@gmail.com>
2021-09-15 15:15:01 -0300

    Refactorización básica

---
+++ src/mhz19/ESP32/basic/secrets.h
@@ -0,0 +1,10 @@
+/*
+
+  secrets.h - Use this file to store the private credentials 
+         and connection details
+
+*/
+
+#define SECRET_SSID "MySSID"      // replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"  // replace MyPassword with your WiFi password
+
